03f40bbaa0d00ee4202e524811f78604c3960fba
muskan-coder10 <muskanmishra200610.gmail.com>
2026-07-23 13:32:27 +0530

    fix: use env variable for API and socket URL

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+ baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 //  token is attached automatically when logged in
